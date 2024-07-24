e85f4b0751335f5b3bec4166a54faabb824a107f
Zena-park <zena@tokamak.network>
2024-07-24 13:54:46 +0900

    test: needCheckpointCount, globalCheckpoint(count)

--- hardhat.config.js
+++ hardhat.config.js
@@ -47,14 +47,18 @@ module.exports = {
       accounts: {
         count: 5,
       },
-      forking: {
-        url: `https://mainnet.infura.io/v3/${process.env.InfuraKey}`,
-        blockNumber: 20310242,
-      },
+      // forking: {
+      //   url: `https://mainnet.infura.io/v3/${process.env.InfuraKey}`,
+      //   blockNumber: 20310242,
+      // },
       // forking: {
       //   url: `https://sepolia.infura.io/v3/${process.env.InfuraKey}`,
       //   // blockNumber: 15125232,
       // },
+      forking: {
+        url: `https://sepolia.infura.io/v3/${process.env.InfuraKey}`,
+        blockNumber: 6365718,
+      },
     },
     local: {
       chainId: 31337,
